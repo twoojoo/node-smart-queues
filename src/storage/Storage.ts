9e309187c610ad9574b1a9e044b2a972d85c4c03
Giovanni Campeol <giovanni@smartpricing.it>
2023-03-19 08:57:51 +0100

    added comments to storage model

--- src/storage/Storage.ts
+++ src/storage/Storage.ts
@@ -12,11 +12,15 @@ export abstract class Storage {
 		return this.name
 	}
 
+	/**Push an item to the storage*/
 	abstract push(key: string, item: QueueItem): Promise<void>
 	
+	/**Shift the first n items in from the storage that have been pushed*/
 	abstract shiftFIFO(key: string, count: number): Promise<StorageShiftOutput>
 
+	/**Shift the last n items in from the storage that have been pushed*/
 	abstract shiftLIFO(key: string, count: number): Promise<StorageShiftOutput>
 
+	/**Get the number of items in the queeue for all the keys*/
 	abstract getStoredCount(): Promise<StoredCount>
 }
