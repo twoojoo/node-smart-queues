053939b62de64204430a28b9f7c344039ea49822
twoojoo <giovanni@smartpricing.it>
2023-05-01 22:40:18 +0200

    url as env var

--- cli/index.ts
+++ cli/index.ts
@@ -1,12 +1,14 @@
+import { colors, nicliPrompt } from "niclijs"
 import { testConnection } from "./request";
-import { nicliPrompt } from "niclijs"
 import { commands } from "./commands";
 
 const DEFAULT_URL = "http://0.0.0.0:3000";
 
 (async function () {
 	//setup server url
-	global.url = (process.argv[2] || DEFAULT_URL) + "/v1/"
+	const baseUrl = process.env.NSQ_URL || DEFAULT_URL
+	console.log(colors.FgLightGrey("connecting to " + baseUrl))
+	global.url = baseUrl + "/v1/"
 
 	//test connection at startup
 	//and every 2 seconds
